67eeb251c8b49715696ccca3e1faf2f242987369
Diane Crame <dcrame@icloud.com>
2020-03-24 15:33:56 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -14,4 +14,10 @@ countdown(10)
 
 def countdown_with_sleep(number)
   while number > 0
-    puts "#{number} seconds(s)"
+    puts "#{number} SECOND(S)!"
+    break
+    number -=1
+  end
+end
+
+countdown_with_sleep(10)
